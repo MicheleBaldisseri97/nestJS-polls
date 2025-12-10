14bee45d908a9697937eb90c0c159db3af5edc8e
michele.baldisseri <baldisseri.michele@gmail.com>
2025-12-10 11:18:17 +0100

    add ControllerAuthGuard for JWT-based authentication

--- server/src/polls/polls.controller.ts
+++ server/src/polls/polls.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { CreatePollDto, JoinPollDto } from './dtos';
 import { PollsService } from './polls.service';
+import { ControllerAuthGuard } from './controller-auth.guard';
+import { RequestWithAuth } from './types';
 
 @Controller('polls')
 export class PollsController {
@@ -17,11 +19,12 @@ export class PollsController {
   }
 
   @Post('/rejoin')
-  rejoin() {
+  @UseGuards(ControllerAuthGuard)
+  rejoin(@Req() request: RequestWithAuth) {
     return this.pollsService.rejoinPoll({
-      name: 'from token',
-      pollID: 'from token',
-      userID: 'from token',
+      name: request.name,
+      pollID: request.pollID,
+      userID: request.userID,
     });
   }
 }
